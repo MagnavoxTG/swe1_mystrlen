2cd4eba2acb050bc10534293421cc49c1fe75dd9
MagnavoxTG <dominik.lohner92@gmx.at>
2019-11-22 19:20:52 +0100

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/mystrlen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mystrlen.dir/main.c.o"
+  "mystrlen"
+  "mystrlen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mystrlen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
